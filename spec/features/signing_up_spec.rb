45307829f1c53d0433957b20c36d30e93631caa0
Fer <sombondrolo@gmail.com>
2022-06-11 20:26:09 -0300

    Add sign in and sign out

--- spec/features/signing_up_spec.rb
+++ spec/features/signing_up_spec.rb
@@ -1,9 +1,9 @@
 require "rails_helper"
 
-RSpec.feature "Users can sign >" do
+RSpec.feature "Users can sign up >" do
   scenario "when providing valid details" do
     visit "/"
-    click_link "Sign Up"
+    click_link "Sign up"
     fill_in "Email", with: "test@example.com"
     fill_in "Password", with: "password"
     fill_in "Password confirmation", with: "password"
