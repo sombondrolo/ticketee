bdaa174e4def6f286479ab0be60f724ad530c243
Fer <sombondrolo@gmail.com>
2022-06-11 19:14:35 -0300

    Added Devise + sign up feature

--- Gemfile
+++ Gemfile
@@ -64,3 +64,5 @@ gem "bootstrap", "~> 5.1"
 gem "font-awesome-rails", "= 4.7.0.6"
 
 gem "bootstrap_form", "= 4.5.0"
+
+gem "devise", "~> 4.7.3"
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
   root "projects#index"
+
   resources :projects do
     resources :tickets
   end
