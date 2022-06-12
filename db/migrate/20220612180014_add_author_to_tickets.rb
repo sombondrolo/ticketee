d5a8f99c83fcd77d791ae38720f4fb81a6c7ef86
Fer <sombondrolo@gmail.com>
2022-06-12 16:08:33 -0300

    Link tickets and users upon ticket creation

--- db/migrate/20220612180014_add_author_to_tickets.rb
+++ db/migrate/20220612180014_add_author_to_tickets.rb
@@ -1,5 +1,5 @@
 class AddAuthorToTickets < ActiveRecord::Migration[6.1]
   def change
-    add_reference :tickets, :author, null: false, foreign_key: { to_table: :users }
+    add_reference :tickets, :author, foreign_key: { to_table: :users }
   end
 end
--- spec/factories/tickets.rb
+++ spec/factories/tickets.rb
@@ -2,5 +2,8 @@ FactoryBot.define do
   factory :ticket do
     name { "Example ticket" }
     description { "An example ticket, nothing more" }
+    association :author, factory: :user
+    # a_different_user = FactoryBot.create(:user, email: "different@example.com")
+    # FactoryBot.create(:ticket, author: a_different_user)
   end
 end
