314fdd2b67838f3f948ddadb943699dfed70577c
Fer <sombondrolo@gmail.com>
2022-06-12 23:17:40 -0300

    Add admin namespace with application controller

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,6 +30,7 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
@@ -63,5 +64,6 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
   #
   config.include Warden::Test::Helpers, type: :feature
+  config.include Warden::Test::Helpers, type: :request
   config.after(type: :feature) { Warden.test_reset! }
 end
---
+++ spec/requests/admin/root_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe "Admin::Applications", type: :request do
+  describe "GET / >" do
+    let(:user) { FactoryBot.create(:user) }
+    let(:admin) { FactoryBot.create(:user, :admin) }
+
+    context "when signed in as a user >" do
+      before do
+        login_as(user)
+      end
+      it "redirects away" do
+        get "/admin"
+        expect(response).to have_http_status(302)
+      end
+    end
+
+    context "when signed in as an admin >" do
+      before do
+        login_as(admin)
+      end
+      it "let the admin in" do
+        get "/admin"
+        expect(response).to have_http_status(:success)
+      end
+    end
+  end
+end
