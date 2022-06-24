480f90559c929666035e88b9f7332c4b79ee9479
Fer <sombondrolo@gmail.com>
2022-06-23 21:00:48 -0300

    Configure Ticketee to run on Github Actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,9 +20,9 @@
 ##  end
 ##end
 
-unless State.exists?
-  State.create(name: "New", color: "#0066CC")
-  State.create(name: "Open", color: "#008000")
-  State.create(name: "Closed", color: "#990000")
-  State.create(name: "Awesome", color: "#663399")
-end
+# unless State.exists?
+#   State.create(name: "New", color: "#0066CC")
+#   State.create(name: "Open", color: "#008000")
+#   State.create(name: "Closed", color: "#990000")
+#   State.create(name: "Awesome", color: "#663399")
+# end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -66,4 +66,8 @@ RSpec.configure do |config|
   config.include Warden::Test::Helpers, type: :feature
   config.include Warden::Test::Helpers, type: :request
   config.after(type: :feature) { Warden.test_reset! }
+
+  if ENV['CI']
+    Capybara.javascript_driver = :selenium_headless
+  end
 end
